f825423a70d856e465c846d3f6450c00e6ad5117
Roaringmind22 <nandor1kovacs@gmail.com>
2021-06-02 19:41:32 +0200

    i can push

--- src/main/java/puppeteer/common/Puppeteer.java
+++ src/main/java/puppeteer/common/Puppeteer.java
@@ -3,7 +3,7 @@ package puppeteer.common;
 import net.fabricmc.api.ModInitializer;
 
 public class Puppeteer implements ModInitializer {
-
+  //yes i can push
     public static final String MODID = "puppeteer"; /// it's this ok ?
 
     @Override
